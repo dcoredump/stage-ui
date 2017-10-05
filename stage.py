4a6b309e3f6054d07fdd1000236facbca21cfb21
Holger Wirtz <dcoredump@googlemail.com>
2017-10-05 17:01:49 +0200

    Using HTML for pedalboards.

--- stage.py
+++ stage.py
@@ -24,15 +24,24 @@ PEDALBOARDS_PATH=os.environ['HOME']+"/.pedalboards"
 def main_gui():
     global tab_index_group,tab_box
 
-    # Pedalboards container
-    pedalboards_container = gui.Table(width=1280,height=800)
+# Pedalboards container ######################################################
+    pedalboard_header="""
+<h1>Pedalboards</h1>
+<form id='form'>
+    """
+    pedalboard_footer="""
+    """
+    pedalboard_pedalboards="""
+<table style='border:1px; border-color: #000088; background: #ccccff; margin: 8px; padding: 8px;'>
+    """
     for p in get_pedalboard_names():
-        pedalboards_container.tr()
-        pb=gui.Button(p)
-        pedalboards_container.td(pb)
-        pedalboards_container.td(gui.Spacer(500,10))
+        pedalboard_pedalboards+="<tr><td><input type='button' value='"+p+"' onclick='load_pedalboard(p)'>"
+    pedalboard_pedalboards+="</table>"
 
-    # System container
+    pedalboard_html=pedalboard_header+pedalboard_pedalboards+pedalboard_footer
+    pedalboards_container = html.HTML(pedalboard_html,align=-1,valign=-1,width=1280,height=1000)
+
+# System container ######################################################
     system_header="""
 <h1>System</h1>
 <form id='form'>
@@ -56,6 +65,8 @@ def main_gui():
     """
     system_html=system_header+system_samplerate_table+system_buffers_table+system_footer
     system_container = html.HTML(system_html,align=-1,valign=-1,width=1280,height=1000)
+
+# Tabs container ######################################################
 
     # Tab index group
     tab_index_group = gui.Group()
@@ -84,6 +95,9 @@ def get_pedalboard_names():
         if(m):
             pedalboards.append(m.group(1))
     return(pedalboards)
+
+def load_pedalboard(pedalboard):
+    print(pedalboard)
 
 ##############################################################################
 # Callback functions
